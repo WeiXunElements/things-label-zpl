82ac29ddb89e0d26f46fa146baf6fce7d5a006f9
lym <lym91@hatiolab.com>
2017-05-26 10:30:27 +0900

    데이터매트릭스 사이즈1일때 수정

--- src/components/barcode.js
+++ src/components/barcode.js
@@ -222,7 +222,8 @@ scene.Barcode.prototype._toZpl = function(T, I) {
     commands.push(['^BU' + orientation, barHeight, showText, textAbove]);
     break;
   case 'datamatrix'      :
-    commands.push(['^BX' + orientation, Math.floor(barWidth * 1.5)]);
+    commands.push(['^BX' + orientation, barWidth == 1 ? 2 : Math.floor(barWidth * 1.5)]);
+		// commands.push(['^BX' + orientation, barWidth]);
     break;
   case 'postal'          :	// bwip에서 지원하지 않는 바코드.
     commands.push(['^BZ' + orientation, barHeight, showText, textAbove]);
